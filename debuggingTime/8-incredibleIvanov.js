2ff4563a32aa6dca2e5215def7229a2e60cb5182
nlane <nlane@udel.edu>
2021-02-25 15:59:47 -0500

    updates workshop link, changes prompt

--- debuggingTime/8-incredibleIvanov.js
+++ debuggingTime/8-incredibleIvanov.js
@@ -1,9 +1,20 @@
-const yourNumber = prompt('Tell me your favorite number...');
-console.log(`
-*********************************************************************
-  The incredible Ivanov will now add 100 to your favorite number...
-  
-  ${100 + yourNumber}
-*********************************************************************
+
+// No need to debug these four lines (this is needed to get user input from the terminal)
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+// Debug the code below
+readline.question('Tell me your favorite number...', yourNumber => {
+    console.log(`
+  *********************************************************************
+    The incredible Ivanov will now add 100 to your favorite number...
+    
+    ${100 + yourNumber}
+  *********************************************************************
 `);
+  readline.close();
+});
+
 console.log();
