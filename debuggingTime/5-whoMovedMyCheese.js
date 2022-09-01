8cfa1c2772109d52fc7a16c4733e9fac89bda819
Louis Rabeno <lrabeno@gmail.com>
2022-09-01 18:08:00 -0400

    part 5 complete

--- debuggingTime/5-whoMovedMyCheese.js
+++ debuggingTime/5-whoMovedMyCheese.js
@@ -1,27 +1,29 @@
 let ingredients = {
   bread: {
-    kind: 'rye',
+    kind: "rye",
   },
   cheese: {
-    kind: 'stilton',
+    kind: "stilton",
   },
   lettuce: {
-    kind: 'butter'
-  }
+    kind: "butter",
+  },
+};
+
+console.log(ingredients);
+
+function sliceCheese(sandwich, cheese) {
+  return "1 slice of" + cheese;
 }
 
-function sliceCheese (sandwich, cheese) {
-  return '1 slice of' + cheese.kind;
-}
-
-function makeASandwich (ingredients) {
-  let sandwich = []
-  sandwich.push(ingredients.bread.kind + ' bread')
+function makeASandwich() {
+  let sandwich = [];
+  sandwich.push(ingredients.bread.kind + " bread");
   sandwich.push(sliceCheese(ingredients.cheese));
   sandwich.push(sliceCheese(ingredients.cheese));
-  sandwich.push(ingredients.lettuce.kind + ' lettuce');
-  sandwich.push(ingredients.bread.kind + ' bread');
+  sandwich.push(ingredients.lettuce.kind + " lettuce");
+  sandwich.push(ingredients.bread.kind + " bread");
   return sandwich;
 }
 
-makeASandwich()
+makeASandwich();
