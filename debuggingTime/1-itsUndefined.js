856619d93c608fa4c6b1c56a630670e8872b496a
Louis Rabeno <lrabeno@gmail.com>
2022-09-01 17:36:08 -0400

    first problem solved

--- debuggingTime/1-itsUndefined.js
+++ debuggingTime/1-itsUndefined.js
@@ -1,13 +1,13 @@
 let cousins = {
   ed: {
-    livesIn: 'Dorchester',
+    livesIn: "Dorchester",
   },
   molly: {
-    livesIn: 'Chennai',
+    livesIn: "Chennai",
   },
   itt: {
-    livesIn: 'A Big Spooky Mansion'
+    livesIn: "A Big Spooky Mansion",
   },
-}
+};
 
-console.log(`Cousin it lives in ${cousins.it.livesIn}`)
+console.log(`Cousin it lives in ${cousins.itt.livesIn}`);
